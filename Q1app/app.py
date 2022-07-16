04a55f61ef67636fd109830008acd25db7561cbe
Alvin Wen <108894936+AlvinWenJH@users.noreply.github.com>
2022-07-17 05:20:42 +0700

    Add files via upload

---
+++ Q1app/app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+import joblib
+import numpy as np
+
+def load_model(model):
+    filename = model+".sav"
+    joblib_model = joblib.load(filename)
+    return joblib_model
+
+
+st.title("Faulty Check Prediction")
+
+st.write("""### We need some information to predict the Faulty""")
+
+models = ("RandomForest", "XGBoost","MLP")
+modelname = st.selectbox("Model :",models)
+
+col1, col2 = st.columns(2)
+with col1:
+  CH1Temp1 = st.number_input("CH1Temp1")
+  CH1Temp2 = st.number_input("CH1Temp2")
+  CHP1Vib1 = st.number_input("CHP1Vib1")
+  CHP1Vib2 = st.number_input("CHP1Vib2")
+with col2:
+  CH2Temp1 = st.number_input("CH2Temp1")
+  CH2Temp2 = st.number_input("CH2Temp2")
+  CHP2Vib1 = st.number_input("CHP2Vib1")
+  CHP2Vib2 = st.number_input("CHP2Vib2")
+ok = st.button("Check")
+if ok:
+  model = load_model(modelname)
+  X = np.array([CH1Temp1,CH1Temp2,CH2Temp1,CH2Temp2,
+  CHP1Vib1,CHP1Vib2,CHP2Vib1,CHP2Vib2])
+  X = X.reshape(-1,8)
+  ypred = model.predict(X)
+  if ypred[0] == 0:
+    ans = 'Not Faulty'
+  else:
+    ans = 'Faulty'
+  st.subheader(f"The result is {ans}")
+
